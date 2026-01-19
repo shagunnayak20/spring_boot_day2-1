2292375d0859f02e44c736069114759b2edae9cc
Shagun Nayak <shagun.nayak_cs23@gla.ac.in>
2026-01-19 15:16:56 +0530

    more info

--- revision/src/main/java/com/example/demo/controller/Home.java
+++ revision/src/main/java/com/example/demo/controller/Home.java
@@ -4,11 +4,19 @@ import com.example.demo.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class Home {
     @GetMapping("/about")
-    public StudentModel getStudent(){
-        StudentModel s=new StudentModel(1,"shagun","shagun.gmail.com");
-        return s;
+    public ArrayList<StudentModel> getStudent(){
+        StudentModel s=new StudentModel();
+        ArrayList<StudentModel> a=new ArrayList<>();
+        a.add(new StudentModel(1,"Shagun","shagun.gmail.com"));
+        a.add(new StudentModel(2,"Mahak","Mahak.gmail.com"));
+        a.add(new StudentModel(3,"Shreya","shreya.gmail.com"));
+        a.add(new StudentModel(4,"gunajn","gunjan.gmail.com"));
+        a.add(new StudentModel(5,"suhani","shuhani.gmail.com"));
+        return a;
     }
 }
